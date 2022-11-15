0f891aae17c3ca83646c668a8b2d357de24664f9
aryabhatalabs <43720330+aryabhatalabs@users.noreply.github.com>
2022-11-15 13:43:03 +0530

    Update receiver.js

--- CastReceiver-master/js/receiver.js
+++ CastReceiver-master/js/receiver.js
@@ -408,7 +408,7 @@ playerManager.setMessageInterceptor(
   cast.framework.messages.MessageType.LOAD, loadRequestData => {
     castDebugLogger.debug(LOG_RECEIVER_TAG,
       `loadRequestData: ${JSON.stringify(loadRequestData)}`);
-//       loadRequestData.mediaSessionId=890;
+      loadRequestData.mediaSessionId=890;
       loadRequestData.requestId=190;
       globalLoadRequestData=loadRequestData;
 //      loadRequestData.media.hlsSegmentFormat = cast.framework.messages.HlsSegmentFormat.TS;  
